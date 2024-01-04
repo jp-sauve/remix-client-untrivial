89e163156452651e1d376cd0dba1012ee1e72ad0
JP Sauve <jean-paul@grokology.ca>
2024-01-03 23:25:19 -0500

    server stuff

--- app/root.jsx
+++ app/root.jsx
@@ -1,18 +1,20 @@
-import { Links, Meta, Outlet, Scripts } from "@remix-run/react";
+import { Links, LiveReload, Meta, Outlet, Scripts } from "@remix-run/react";
 
 export default function App() {
   return (
-    <html>
+    <html lang="en">
       <head>
         <link rel="icon" href="data:image/x-icon;base64,AA" />
         <Meta />
         <Links />
       </head>
       <body>
-        <h1>Hello world!</h1>
+        <h1>Hello world!!</h1>
+        <h2>Howareyou.Good.</h2>
         <Outlet />
 
         <Scripts />
+        <LiveReload />
       </body>
     </html>
   );
