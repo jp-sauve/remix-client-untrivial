9b8537ccd8144e898f1f241f72ddd53e82b74c19
JP Sauve <jean-paul@grokology.ca>
2024-01-10 22:37:51 -0500

    adds auth module

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import { Form, Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 import appStylesHref from "./app.css";
 export const links: LinksFunction = () => [
@@ -8,13 +8,41 @@ export default function App() {
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="data:image/x-icon;base64,AA" />
         <Meta />
         <Links />
       </head>
       <body>
-        <h1>Hello world!!</h1>
-        <h2>Howareyou.Good.</h2>
+        
+        <div id="sidebar">
+          <h1>Untrivial 1.0</h1>
+          <div>
+            <Form id="search-form" role="search">
+              <input
+                aria-label="Search contacts"
+                id="q"
+                name="q"
+                placeholder="Search"
+                type="search"
+              />
+              <div aria-hidden hidden={true} id="search-spinner" />
+            </Form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <a href={`/contacts/1`}>Your Name</a>
+              </li>
+              <li>
+                <a href={`/contacts/2`}>Your Friend</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
